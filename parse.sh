53917705bb85e9e99b3ca884dd2fb7df22e14112
asarch <asarch@hotmail.com>
2019-10-24 02:34:12 -0500

    Final commit

--- parse.sh
+++ parse.sh
@@ -1,30 +1,18 @@
 #!/bin/ksh
 
-# CREATE TABLE "categoria" ( "id" INTEGER PRIMARY KEY AUTOINCREMENT, "nombre" TEXT );
-# CREATE TABLE "paquete" ( "id" INTEGER PRIMARY KEY AUTOINCREMENT, "nombre" TEXT, "descripcion" TEXT, "categoria" INTEGER REFERENCES categoria(id) ON DELETE CASCADE ON UPDATE CASCADE );
+if [ ! -d /usr/ports ]; then
+	echo "Ports base dir cannot found"
+	exit 1
+fi
 
-#if [ ! -d /usr/ports ]; then
-#	echo "Ports base dir cannot found"
-#	exit 1
-#fi
+for categoria in $( find /usr/ports -maxdepth 1 -type d ! -name . ! -name ports ! -name CVS); do
+	nombre_categoria=$( basename $categoria )
 
-#if [ ! -f ports.db ]; then
-#	sqlite3 ports.db "CREATE TABLE \"categoria\" ( \"id\" INTEGER PRIMARY KEY AUTOINCREMENT, \"nombre\" TEXT );"
-#	sqlite3 ports.db "CREATE TABLE \"paquete\" ( \"id\" INTEGER PRIMARY KEY AUTOINCREMENT, \"nombre\" TEXT, \"descripcion\" TEXT, \"categoria\" INTEGER REFERENCES categoria(id) ON DELETE CASCADE ON UPDATE CASCADE );"
-#fi
+	echo "Creating category $nombre_categoria"
+	psql -U asarch ports -c "INSERT INTO categoria (nombre) VALUES ('$nombre_categoria');"
+done
 
-#for categoria in $( find /usr/ports -maxdepth 1 -type d ! -name . ! -name ports ! -name CVS); do
-#	nombre=$( basename $categoria )
-#	sqlite3 ports.db "INSERT INTO categoria ( nombre ) VALUES ( \"$nombre\" )"
-
-#	echo "Creating category $nombre"
-#	psql -U asarch ports -c "INSERT INTO categoria ( nombre ) VALUES ( '$nombre' );"
-#done
-
-#exit 1
-
-#for categoria in $( sqlite3 ports.db "SELECT * FROM categoria;" ); do
-for categoria in $( psql -U asarch ports -c "SELECT * FROM categoria;" ); do
+for categoria in $( psql --no-align --tuples-only -U asarch ports -c "SELECT * FROM categoria;" ); do
 	indice_categoria=$( echo $categoria | awk -F\| '{ print $1 }' )
 	nombre_categoria=$( echo $categoria | awk -F\| '{ print $2 }' )
 
@@ -39,15 +27,7 @@ for categoria in $( psql -U asarch ports -c "SELECT * FROM categoria;" ); do
 
 		echo "Processing package $nombre_categoria/$nombre_paquete"
 
-		#psql -U asarch ports -c "INSERT INTO paquete ( nombre, descripcion, categoria ) VALUES ( '$nombre_paquete', '$descripcion', $indice_categoria );"
 		echo "INSERT INTO paquete (nombre, descripcion, categoria) VALUES ('$nombre_paquete', :'descripcion', $indice_categoria);" | psql -v descripcion="$descripcion" -f - -U asarch -d ports
 
-#		if [ echo $descripcion | grep "'" ]; then
-#			query="INSERT INTO paquete ( nombre, descripcion, categoria ) VALUES ( \"$nombre_paquete\", \"$descripcion\", $indice_categoria );"
-#		else
-#			query="INSERT INTO paquete ( nombre, descripcion, categoria ) VALUES ( '$nombre_paquete', '$descripcion', $indice_categoria );"
-#		fi
-
-#		sqlite3 ports.db $query
 	done
 done
